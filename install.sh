0d39413120ffa398af58e8e3e7d1146170f4c812
Benny <bennypythoncoder@gmail.com>
2021-05-01 15:31:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,8 +49,15 @@ sed -i 's:server.error-handler-404    = "/pihole/index.php/":server.error-handle
 echo "[✓] Changed configuration file for lighttpd located at /etc/lighttpd/lighttpd.conf"
 sudo service lighttpd restart
 
-### Changes the blocking mode to IP insteado of default "" in config file at /etc/pihole/pihole-FTL.conf
-sed -i 's/BLOCKINGMODE=/BLOCKINGMODE=IP/gi' /etc/pihole/pihole-FTL.conf
+### Checks if /etc/pihole/pihole-FTL.conf exists. If it does, changes the blocking mode to IP instead of default NULL in config file at /etc/pihole/pihole-FTL.conf
+FILE=/etc/pihole/pihole-FTL.conf
+if [ -f "$FILE" ]; then
+    # File exists
+    sed -i 's/NULL/IP/gi' /etc/pihole/pihole-FTL.conf
+else 
+    # File does not exist
+    sudo echo "BLOCKINGMODE=IP" >> /etc/pihole/pihole-FTL.conf
+fi
 sudo service pihole-FTL restart
 
 ### Start Email Checker program in background, make it check email every 10 seconds
