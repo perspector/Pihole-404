f7f311e19c6c010765109898103eb52937e2346b
Benny <bennypythoncoder@gmail.com>
2021-05-01 11:26:36 -0400

    Update CustomBlockPage.php

--- CustomBlockPage.php
+++ CustomBlockPage.php
@@ -17,7 +17,7 @@
       <br>
       <br>
       If you believe that this page has been blocked in error,<br>please contact me
-      <a href="mailto:bennypythoncoder@gmail.com?subject=Domain%20Blocked&body=[sub]" onclick="this.href =this.href.replace('[sub]',window.location)" target="_blank" rel=noopener noreferrer><button style="background-color:white; border-color:white">here</button></a>.<br>
+      <a href="mailto:someone@example.com?subject=Domain%20Blocked&body=[sub]" onclick="this.href =this.href.replace('[sub]',window.location)" target="_blank" rel=noopener noreferrer><button style="background-color:white; border-color:white">here</button></a>.<br>
       Do <u><strong>not</strong></u> change the message or subject!<br>A script will automaticly whitelist the domain.
    </center>
 </center>
