a5911a0f30d900e47f2c8815e385f0c1a4fbdc03
Benny <bennypythoncoder@gmail.com>
2021-05-01 11:24:55 -0400

    Add files via upload

---
+++ CustomBlockPage.php
@@ -0,0 +1,26 @@
+<html>
+<title>BLOCKED</title>
+<center>
+<body bgcolor=000000 text=FFFFFF style="background-image: url('/pihole/background.jpg'); background-repeat: no-repeat; background-size: cover;">
+   <center>
+      <img src="pihole/Astronaut1.png" width=150><br>
+      <font size=+8 face="monoid">
+         <strong>404</strong><br>
+      </font>
+      <font size=+2>
+         <br>
+         This Page Has Been Blocked<br>
+         *OR* Does Not Exist<br>
+      </font>
+      <br>
+      Notice: Some pages have been blocked due to content or bandwidth restrictions.
+      <br>
+      <br>
+      If you believe that this page has been blocked in error,<br>please contact me
+      <a href="mailto:bennypythoncoder@gmail.com?subject=Domain%20Blocked&body=[sub]" onclick="this.href =this.href.replace('[sub]',window.location)" target="_blank" rel=noopener noreferrer><button style="background-color:white; border-color:white">here</button></a>.<br>
+      Do <u><strong>not</strong></u> change the message or subject!<br>A script will automaticly whitelist the domain.
+   </center>
+</center>
+</body>
+</center>
+</html>
