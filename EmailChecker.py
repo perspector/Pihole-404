4fd380ce03f825d20e26c07e35ca8c08bef5f239
Benny <bennypythoncoder@gmail.com>
2021-05-01 10:20:46 -0400

    Update EmailChecker.py

--- EmailChecker.py
+++ EmailChecker.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+
+# imports necessary libraries
 from imap_tools import MailBox, AND
 from time import sleep
 import os
 
-#sleep(10) # Checks every 10 seconds
-# get list of email subjects from INBOX folder
-with MailBox('imap.gmail.com').login("someone@example.com", "password", initial_folder='INBOX') as mailbox:
+# gets list of email subjects from INBOX folder
+with MailBox('imap.gmail.com').login("someone@example.com", "PASSWORD", initial_folder='INBOX') as mailbox:
     body = [msg.text for msg in mailbox.fetch(AND(subject='Domain Blocked'))]
+    
+    # Checkes if the folder PiHoleWhitelist exists
+    folder_exists = mailbox.folder.exists('PiHoleWhitelist')
+    # if it does not exist, create it
+    if folder_exists == False:
+        mailbox.folder.create('PiHoleWhitelist')
+    # Moves email to PiHoleWhitelist folder to avoid a cluttered Inbox
     mailbox.move(mailbox.fetch(AND(subject='Domain Blocked')), 'PiHoleWhitelist')
     try:
         body = body[0]
