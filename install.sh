5997dd6cea4408a303b1f91cb3cf3b2fbaf73e70
Benny <bennypythoncoder@gmail.com>
2021-05-02 08:41:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # cool "Pihole-404" text
 apt-get install figlet cowsay lolcat
 clear
-figlet -f slant "Pihole-404" | lolcat -p 1.75 -S 45 -f | cowsay -f tux -n 
+figlet -f slant -k "Pihole-404" | lolcat -p 1.75 -S 45 -f | cowsay -f tux -n 
 
 # Installs dependency for the Python script
 pip3 install imap-tools
